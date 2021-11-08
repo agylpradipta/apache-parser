e8e562755650c575ac4d93226d08dd4faeee44a8
agylpradipta <agylpradipta70@gmail.com>
2021-11-08 14:22:57 +0700

    Add Log Parser

---
+++ apache-500.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+date_apache=`date --date='10 minutes ago' '+%d/%b/%Y:%H:%M'`
+date_file=`date --date='10 minutes ago' '+%d-%b-%Y-%H-%M'`
+
+for f in "$@";do
+  if [[ "$f" == access.log* ]];then
+     awk '($9 ~ /500/)' "$f" | grep -A 9999 ${date_apache} | awk '{print $7}' | sort | uniq -c | sort -rn >> apache-500-${date_file}.log
+  fi
+done
